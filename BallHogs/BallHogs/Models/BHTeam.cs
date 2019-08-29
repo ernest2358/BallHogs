66ddb8ccc5c85d6d6f3ab8b4740ed3ce0cccbc14
Hermann <hmcenhill@gmail.com>
2019-08-29 19:56:38 -0400

    Add Team,manager,teammanager, and dataContext to Models

---
+++ BallHogs/BallHogs/Models/BHTeam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BallHogs.Models
+{
+    public class BHTeam
+    {
+        public int BHTeamId { get; set; }
+        public string TeamName { get; set; }
+        public string ManagerName { get; set; }
+
+
+        public ICollection<Datum> Players { get; set; }
+
+    }
+}
---
+++ BallHogs/BallHogs/Models/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BallHogs.Models
+{
+    public class Manager
+    {
+        public int ManagerID { get; set; }
+        public string UserName { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public ICollection<BHTeam> BHTeams { get; set; }
+
+        public Manager(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
