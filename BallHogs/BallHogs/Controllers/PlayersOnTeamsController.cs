1980426a8dce2293213cdc4309ace8c42cb5fe02
ernest2358 <ernest.h.bell@gmail.com>
2019-09-05 16:50:57 -0400

    Fixed redirection when adding players to a BHTeam

--- BallHogs/BallHogs/Controllers/PlayersOnTeamsController.cs
+++ BallHogs/BallHogs/Controllers/PlayersOnTeamsController.cs
@@ -39,7 +39,7 @@ namespace BallHogs.Controllers
             var client = _httpClientFactory.CreateClient();
             client.BaseAddress = new Uri("https://www.balldontlie.io");
 
-            var response = await client.GetAsync($"/api/v1/players?search={player}&season=2018");
+            var response = await client.GetAsync($"/api/v1/players?search={player}");
 
             var body = await response.Content.ReadAsStringAsync();
 
@@ -98,8 +98,7 @@ namespace BallHogs.Controllers
                 _context.Add(playerOnTeam);
                 await _context.SaveChangesAsync();
 
-                return RedirectToAction("Index", "Home");
-                // return RedirectToAction("Details", "BHTeam", new { id = teamID });  I see this here 
+                return RedirectToAction("Details", "BHTeams", new {id = teamID });
             }
             else
             {
